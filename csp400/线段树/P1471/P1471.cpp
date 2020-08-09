13fa43c7f9746950075e0b84020bd05a70526233
529680697 <czy000123@qq.com>
2020-08-09 14:23:54 +0800

    add 线段树(方差) P1471

--- csp400/线段树/P1471/P1471.cpp
+++ csp400/线段树/P1471/P1471.cpp
@@ -6,8 +6,8 @@ const int maxn = 100005;
 
 struct node
 {
-    int l, r, lz;
-    double sum, pf;
+    int l, r;
+    double sum, pf, lz;
 } tree[maxn << 2];
 int n, m, op, x, y;
 double a[maxn], k;
@@ -20,8 +20,8 @@ void push_down(int i)
         tree[l].lz += tree[i].lz; //lazy_tag下放到左右孩子
         tree[r].lz += tree[i].lz;
         int mid = (tree[i].l + tree[i].r) >> 1;
-        tree[l].pf += ((tree[i].lz * tree[l].sum) * 2) + (mid - tree[l].l + 1) * tree[i].lz * tree[i].lz;
-        tree[r].pf += ((tree[i].lz * tree[r].sum) * 2) + (tree[r].r - mid) * tree[i].lz * tree[i].lz;
+        tree[l].pf += tree[i].lz * tree[l].sum * 2 + (mid - tree[l].l + 1) * tree[i].lz * tree[i].lz;
+        tree[r].pf += tree[i].lz * tree[r].sum * 2 + (tree[r].r - mid) * tree[i].lz * tree[i].lz;
         tree[l].sum += tree[i].lz * (mid - tree[l].l + 1); //左右孩子节点更新区间和
         tree[r].sum += tree[i].lz * (tree[r].r - mid);
         tree[i].lz = 0; //清空父亲节点lazy_tag
@@ -44,17 +44,17 @@ void build_tree(int i, int l, int r)
     tree[i].pf = tree[i << 1].pf + tree[(i << 1) | 1].pf;
 }
 
-double search_a(int i, int l, int r) //查询平均值
+double search_a(int i, int l, int r) //查询和
 {
     if (tree[i].l >= l && tree[i].r <= r)
         return tree[i].sum;
     if (tree[i].r < l || tree[i].l > r)
         return 0;
-    if (tree[i].lz > 0)
-        push_down(i);
+    // if (tree[i].lz > 0)
+    push_down(i);
     double ans = 0;
     if (tree[i << 1].r >= l)
-        ans += search_a(i << 1, l, r);
+        ans = search_a(i << 1, l, r);
     if (tree[i << 1 | 1].l <= r)
         ans += search_a(i << 1 | 1, l, r);
     return ans;
@@ -66,28 +66,30 @@ double search_pf(int i, int l, int r) //查询平方和
         return tree[i].pf;
     if (tree[i].r < l || tree[i].l > r)
         return 0;
-    if (tree[i].lz > 0)
-        push_down(i);
+    // if (tree[i].lz > 0)
+    push_down(i);
     double ans = 0;
     if (tree[i << 1].r >= l)
-        ans += search_a(i << 1, l, r);
+        ans = search_pf(i << 1, l, r);
     if (tree[i << 1 | 1].l <= r)
-        ans += search_a(i << 1 | 1, l, r);
+        ans += search_pf(i << 1 | 1, l, r);
     return ans;
 }
 
-void add2(int i, int l, int r, int k) //区间修改
+void add2(int i, int l, int r, double k) //区间修改
 {
+    if (tree[i].r < l || r < tree[i].l)
+        return;
     if (tree[i].r <= r && tree[i].l >= l)
     {
-        tree[i].pf += tree[i].sum * k * 2 + (tree[i].r - tree[i].l + 1) * k * k;
+        tree[i].pf += tree[i].sum * k * 2.0 + (tree[i].r - tree[i].l + 1) * k * k;
         tree[i].sum += k * (tree[i].r - tree[i].l + 1);
         tree[i].lz += k;
     }
     else
     {
-        if (tree[i].lz > 0)
-            push_down(i);
+        // if (tree[i].lz > 0)
+        push_down(i);
         if (tree[i << 1].r >= l)
             add2(i << 1, l, r, k);
         if (tree[(i << 1) | 1].l <= r)
@@ -111,6 +113,7 @@ int main()
         if (op == 1)
         {
             scanf("%d%d%lf", &x, &y, &k);
+            // printf("%.10lf\n", k);
             add2(1, x, y, k);
         }
         else if (op == 2)
@@ -122,7 +125,7 @@ int main()
         {
             scanf("%d%d", &x, &y);
             double avg = search_a(1, x, y) / ((y - x + 1) * 1.0);
-            printf("%.4lf\n", search_pf(1, x, y) / ((y - x + 1) * 1.0) - avg * avg);
+            printf("%.4lf\n", (search_pf(1, x, y) / ((y - x + 1) * 1.0)) - (avg * avg));
         }
     }
 
