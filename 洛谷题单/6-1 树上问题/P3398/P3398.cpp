480ef475b927348c9939ef3e0c959c4180aa980f
529680697 <czy000123@qq.com>
2020-09-04 10:50:00 +0800

    add 题单 树上交点 P3398

---
+++ 洛谷题单/6-1 树上问题/P3398/P3398.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+const int inf = 0x3f3f3f3f;
+const int maxn = 1e5 + 5;
+
+struct node
+{
+    int v, nxt;
+} no[maxn << 1];
+
+int head[maxn], total_edge, d[maxn], st[maxn][20];
+int n, q;
+
+inline void dfs(int u, int fa)
+{
+    d[u] = d[fa] + 1;
+    st[u][0] = fa;
+    for (int i = 1; (1 << i) <= d[u]; i++)
+        st[u][i] = st[st[u][i - 1]][i - 1];
+    for (int i = head[u]; i; i = no[i].nxt)
+    {
+        int v = no[i].v;
+        if (v != fa)
+            dfs(v, u);
+    }
+}
+
+inline int lca(int a, int b)
+{
+    if (d[a] > d[b]) //保证a在b上方，即a的深度小于b
+        swap(a, b);
+    for (int i = 16; i >= 0; i--) //将b向上移到与a相同深度，从大到小依次试探
+        if (d[a] <= d[b] - (1 << i))
+            b = st[b][i];
+    if (a == b) //a为两者祖先
+        return a;
+    for (int i = 16; i >= 0; i--)
+    {
+        if (st[a][i] == st[b][i]) //a、b向上移2^i深度之后祖先相同
+            continue;
+        else //a、b向上移2^i深度后祖先不相同
+            a = st[a][i], b = st[b][i];
+    }
+    return st[a][0]; //此时，a、b已经不能再上移，因此返回a的父亲即可
+}
+
+inline void add_edge(int u, int v)
+{
+    no[++total_edge].v = v;
+    no[total_edge].nxt = head[u];
+    head[u] = total_edge;
+}
+
+int main()
+{
+    int u, v, a, b, c, _d;
+    freopen("in", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> q;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> u >> v;
+        add_edge(u, v), add_edge(v, u);
+    }
+    dfs(1, 0);
+    for (int i = 0; i < q; i++)
+    {
+        cin >> a >> b >> c >> _d;
+        int f1 = lca(a, b), f2 = lca(c, _d);
+        if (d[f1] > d[f2])
+        {
+            swap(f2, f1), swap(c, a), swap(_d, b);
+        }
+        if (lca(f2, a) == f2 || lca(f2, b) == f2)
+            cout << "Y" << endl;
+        else
+            cout << "N" << endl;
+    }
+
+    return 0;
+}
